c1228618301bdd5931ff3b740ee02161ff980c3c
Joseph Iglecias <igleciasjoseph@gmail.com>
2019-07-07 02:59:16 -0400

    forgot to return self to allow chaining

--- user.py
+++ user.py
@@ -3,15 +3,19 @@ class user:
     def __init__(self, name, balance):
         self.name = name
         self.balance = balance
+        return self
 
     def makeDeposit(self, amount):
         self.balance += amount
+        return self
 
     def makeWithdrawal(self, amount):
         self.balance -= amount
+        return self
 
     def displayBalance(self):
         print(self.balance)
+        return self
 
     def transferMoney(self, amount, other):
         if amount > self.balance:
@@ -21,6 +25,7 @@ class user:
             self.balance -= amount
             other.balance += amount
             # print(f'New balance for {other.name} is {other.balance}')
+        return self
 
 
 mike = user('mike', 500)
